52f114b444a088e0bb77ec475dceebe38110859c
gustavadolf10 <167212860+gustavadolf10@users.noreply.github.com>
2026-04-18 23:24:41 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ function getQueryParams() {
   return params;
 }
 
-let para = "Mayte"; // valor por defecto
+let para = "Daniela"; // valor por defecto
 let mensaje = `Ella dice que no es bonita, pero la verdad es que es hermosa. No entiendo
 cómo no puedes ver tu belleza cuando para mí eres perfecta. Cada vez
 que miro tu foto en mi celular al amanecer, siento que el día empieza de la
@@ -144,7 +144,7 @@ let mensajeAnim = {
   timer: null,
   velocidad: 70 // ms por letra (antes 28)
 };
-let loadingText = "Para ti Mayte "; // valor por defecto para loading
+let loadingText = "Para ti Daniela "; // valor por defecto para loading
 
 // --- Papel 3D DOM y animación ---
 const papel3d = document.getElementById('papel3d');
@@ -789,7 +789,7 @@ window.addEventListener('DOMContentLoaded', () => {
   if (params.loading) {
     loadingText = params.loading;
   } else {
-    loadingText = "Para ti Mayte";
+    loadingText = "Para ti Daniela";
   }
 
   setCarta3D(0);
